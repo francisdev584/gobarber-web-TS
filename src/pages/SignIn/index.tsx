7f16c4fead7b5cdf1d9e913b78178dd85815dd84
Francinildo M. Rodrigues <francis584@gmail.com>
2020-10-17 12:02:26 -0300

    Validation Login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {useCallback,useRef} from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import {Form} from '@unform/web';
+import{FormHandles} from '@unform/core';
+import * as Yup from 'yup';
 
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string()
+        .required('Senha obrigatória'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      console.log(err);
+
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return(
   <Container>
     <Content>
       <img src={logoImg} alt="GoBarber" />
 
-      <form>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <h1>Faça seu logon</h1>
 
         <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -28,7 +57,7 @@ const SignIn: React.FC = () => (
         <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
-      </form>
+      </Form>
       <a href="forgot">
         <FiLogIn />
         Criar Conta
@@ -37,5 +66,6 @@ const SignIn: React.FC = () => (
     <Background />
   </Container>
 );
+}
 
 export default SignIn;
