ad5f1435e7a63911d9c811226556b7f69cbe8a34
Francinildo M. Rodrigues <francis584@gmail.com>
2020-10-22 16:52:00 -0300

    Toast Style Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,19 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import ToastContainer from './components/ToastContainer';
 import { AuthProvider } from './hooks/AuthContext';
 
 import GlobalStyle from './styles/global';
 
-const App: React.FC = () => {
-  return (
-    <>
-      <AuthProvider>
-        <SignIn />
-      </AuthProvider>
-
-      <GlobalStyle />
-    </>
-  );
-};
+const App: React.FC = () => (
+  <>
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
+    <ToastContainer />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
